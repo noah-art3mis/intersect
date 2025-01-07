1d259701e26bba1e5014f7a968c75c22b4df99d4
Gustavo Costa <gustavo@arcos.org.br>
2025-01-07 01:26:10 +0000

    fix urls

--- intersect/transform.py
+++ intersect/transform.py
@@ -1,23 +1,49 @@
+import os
 import pandas as pd
 import numpy as np
 from selectolax.lexbor import LexborHTMLParser
+from selectolax.lexbor import LexborNode
+from urllib.parse import urljoin
 
 OUTPUT_PATH = "intersect/data/jobs2.feather"
 
 
-def load(data: list, path: str):
+def load(data: list, path: str) -> None:
     df = pd.DataFrame(data)
     df.to_feather(path)
 
 
-def transform_cvlibrary(html: str):
+def get_url(article: LexborNode) -> str | None:
+    BASE_URL = "https://www.cv-library.co.uk/"
+
+    element = article.css_first(".job__description-more")
+
+    if element is None:
+        return None
+
+    path = element.attributes.get("href")
+
+    if path is None:
+        return None
+
+    return urljoin(BASE_URL, path)
+
+
+def not_ad(item: LexborNode) -> bool:
+    """Some elements are ads and can be filtered out by observing their lack of data."""
+    element = item.css_first("article.job").css_first(".job__description-more")
+    return True if element is not None else False
+
+
+def transform_cvlibrary(html: str) -> list:
     parser = LexborHTMLParser(html)
     results = parser.css("li.results__item")
-    return [transform_item_cvlibrary(x) for x in results]
+    return [transform_item_cvlibrary(x) for x in results if not_ad(x)]
 
 
-def transform_item_cvlibrary(result):
-    article = result.css_first("article.job")
+def transform_item_cvlibrary(item: LexborNode) -> dict:
+
+    article = item.css_first("article.job")
 
     return {
         "title": article.attributes.get("data-job-title"),
@@ -27,9 +53,8 @@ def transform_item_cvlibrary(result):
         "type": article.attributes.get("data-job-type"),
         "posted": article.attributes.get("data-job-posted"),
         "job_industry": article.attributes.get("data-job-industry"),
-        # abstract
-        # full description
-        # url
+        "abstract": article.css_first(".job__description").text().strip(),
+        "url": get_url(article),
     }
 
 
